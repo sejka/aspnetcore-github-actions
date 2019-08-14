1dfecf4ead49894006b648725e09c9e7dfe2f841
Karol Sejka <teherty@gmail.com>
2019-08-14 17:49:03 +0200

    Initial asp.net core webapp commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcore_github_actions.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
